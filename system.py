e275c323f2e1b2b5bb1ec9c621de4baa47e5885c
svervisc <svervisc>
2020-05-12 09:29:00 +0200

    Initial commit

---
+++ system.py
@@ -0,0 +1,26 @@
+a = 1.1
+
+def initialize():
+    global x, result
+    x = 1.
+    result = [x]
+
+
+def observe():
+    global x, result
+    result.append(x)
+
+
+
+
+def update():
+    global x, result
+    x = a * x
+
+initialize()
+for t in range(30):
+    update()
+    observe()
+
+print(result)
+        
